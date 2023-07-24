1a7236b724be7899052356aa34444b8c6ed4f896
edwinmalihu <edwinsimon97@gmail.com>
2023-07-24 23:06:37 +0700

    (add) Connection DB postgres

--- config/database.go
+++ config/database.go
@@ -1,1 +1,37 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func ConnectDb() (*gorm.DB, error) {
+
+	USER := os.Getenv("DB_USER")
+	PASS := os.Getenv("DB_PASS")
+	HOST := os.Getenv("DB_HOST")
+	PORT := os.Getenv("DB_PORT")
+	DBNAME := os.Getenv("DB_NAME")
+	SSLMODE := os.Getenv("SSL")
+	TIMEZONE := os.Getenv("TIMEZONE")
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      false,
+		},
+	)
+
+	url := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s", HOST, USER, PASS, DBNAME, PORT, SSLMODE, TIMEZONE)
+
+	return gorm.Open(postgres.Open(url), &gorm.Config{Logger: newLogger})
+}
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang-service/config"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+type Request struct {
+	TypeCard    string
+	Description string
+}
 
 func main() {
 	fmt.Println("Init Project")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Gagal memuat file .env")
+	}
+
+	db, err := config.ConnectDb()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connection open : ", db)
+
 }
